24e990d3095db02d228b407fd140f0ace472c174
Vladimir I <vladimir.iakovlev@qameta.io>
2024-07-07 14:56:59 +0300

    pp

--- src/main/java/com/bruh/skiff/Main.java
+++ src/main/java/com/bruh/skiff/Main.java
@@ -2,6 +2,6 @@ package com.bruh.skiff;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("ppIsSoft?!");
     }
 }
